f4e29bb0d4ddfa0244a5bedb11e268eb88e483ac
EhsanTgv <EhsanTgv@gmail.com>
2024-02-22 08:36:09 +0300

    Implement loading animation

--- app/src/main/java/com/taghavi/composeloadinganimation/MainActivity.kt
+++ app/src/main/java/com/taghavi/composeloadinganimation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.taghavi.composeloadinganimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.taghavi.composeloadinganimation.ui.theme.ComposeLoadingAnimationTheme
@@ -17,30 +20,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLoadingAnimationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Greeting("Android")
+                    LoadingAnimation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeLoadingAnimationTheme {
-        Greeting("Android")
     }
 }
